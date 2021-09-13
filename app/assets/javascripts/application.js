728ca9dd661ed7bf1a9b24f87e2e9d416d2f9bbd
RY2291 <prgr2498@gmail.com>
2021-09-13 03:48:07 +0000

    [finish]アプリケーションを完成させよう2-9

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //= require jquery3
 //= require popper
-//= require bootstrap-sprodkets
+//= require bootstrap-sprockets
 
 //= require rails-ujs
 //= require activestorage
